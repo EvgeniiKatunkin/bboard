294ca95c6cbc5f799d1ee250a93f1fb177d35eb8
Evgenii <evgeniikatunkin@gmail.com>
2023-04-23 11:25:59 +0200

    LogIn mechanism is created

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Are you activated?')
+    send_messages = models.BooleanField(default=True,
+                                        verbose_name='Do you want to receive messages about new comments?')
+
+    class Meta(AbstractUser.Meta):
+        pass
